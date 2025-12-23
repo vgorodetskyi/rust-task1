29e2ba8fd41a5b5011f4e3d8f9acc54cdeb0e1b5
Vitalii Gorodetskyi <vitalii.gorodetskyi@globallogic.com>
2025-12-23 10:03:53 +0200

    Task 1: Version 2

--- task1/src/main.rs
+++ task1/src/main.rs
@@ -1,31 +1,15 @@
 use std::io;
 use std::collections::HashMap;
 
-fn index_string(data: &str) -> HashMap<&str, Vec<usize>> {
+static SEPARATORS: &[char; 4] = &[' ', ',', ';', '.'];
+fn index_string(string: &str) -> HashMap<&str, Vec<usize>> {
     let mut map: HashMap<&str, Vec<usize>> = HashMap::new();
+    let string_address = string.as_ptr().addr();
 
-    let mut parts = data
-        .split(|c| c == ';' || c == '.' || c == ',' || c == ' ')
-        .filter(|s| !s.is_empty()).collect::<Vec<&str>>();
-    parts.sort();
-    parts.dedup();
-
-    for part in parts {
-        if ! map.contains_key(part) {
-            //println!("  NEW {:?}", part);
-            let mut values : Vec<usize> = Vec::new();
-
-            for (index, matched_str) in data.match_indices(part) {
-                //println!("   Found '{}' at byte index: {}", matched_str, index);
-                values.push(index);
-            }
-            map.insert(part, values);
-        }
-        else {
-            println!("  Update {part} {:?}", map[part]);
-        }
+    for word in string.split(SEPARATORS) {
+        let word_address = word.as_ptr().addr();
+        map.entry(word).or_default().push(word_address - string_address);
     }
-
     return map;
 }
 fn main() {
@@ -50,7 +34,7 @@ fn main() {
         println!("The data_map is '{:#?}'", pre_map);
     }
     else {
-        println!("Hello, please, type your sentece... Press Enter in the end.", predef_string);
+        println!("Hello, please, type your sentece... Press Enter in the end.");
         let data_map = index_string(user_input.as_str());
         println!("The data_map is '{:#?}'", data_map);
     }
@@ -60,7 +44,6 @@ fn main() {
 #[test]
 fn test_short() {
     let map = index_string("a b a");
-    println!("{:?}", map);
     assert_eq!(map["a"], vec![0, 4]);
     assert_eq!(map["b"], vec![2]);
 }
@@ -68,7 +51,6 @@ fn test_short() {
 #[test]
 fn test_long() {
     let map = index_string("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-    println!("{:?}", map);
     assert_eq!(map["Lorem"].len(), 1);
     assert_eq!(map["dolor"].len(), 2);
     assert_eq!(map["dolore"].len(), 2);
@@ -77,6 +59,5 @@ fn test_long() {
 #[test]
 fn test_cyrillic() {
     let map = index_string("привіт привіт");
-    println!("{:?}", map);
     assert_eq!(map["привіт"], vec![0, 13]);
 }
